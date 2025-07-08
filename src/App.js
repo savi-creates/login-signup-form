42436450b511d595ca77a042680298ab542b1505
savi-creates <savannahandresson@gmail.com>
2025-07-08 15:10:45 +0100

    Added a Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import LoginSignup from "./Components/LoginSignup/loginSignup";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <LoginSignup />
+      <Footer />
     </div>
   );
 }
